13b4f15784a69d75c40f6fa78a0d9677a091d5b2
Nils <72611176+Nils92@users.noreply.github.com>
2021-05-03 14:56:07 +0200

    revert last commit

---
+++ WavySpots/WavySpots/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WavySpots
+//
+//  Created by Nils Fjelddahl on 03/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
